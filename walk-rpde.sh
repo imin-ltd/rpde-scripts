a7a0cc1c82849dc8926e00e6882e5da02fc01dac
Alex Coyle <abcoyle@gmail.com>
2017-10-27 17:07:37 +0100

    Only updated items mode (wip)
    Only expected to work in single file mode due to positional options

--- walk-rpde.sh
+++ walk-rpde.sh
@@ -2,23 +2,32 @@
 
 # required commands: awk, curl, jq, tee
 
-if [ "$#" -lt "1" -o "$#" -gt "2" ]
+if [ "$#" -lt "1" -o "$#" -gt "3" ]
 then
-  printf 'Usage: %s <rpde-endpoint> [-s]\n' "$0"
+  printf 'Usage: %s <rpde-endpoint> [-s] [-u]\n' "$0"
   exit 1
 fi
 
 single_file=$2
+only_updated=$3
 max=100
 page=1
 base=$(printf '%s' "$1" | awk -F/ '{print $1 "//" $3}')
+
+if [ "$only_updated" != "" ]
+then
+  jq_program='.items |= map(select(.state == "updated"))'
+else
+  jq_program='.'
+fi
+
 # $1 = next url
 # $2 = num items
 set -- $1 -1
 while [ "$2" -ne "0" ]
 do
   page_padded=$(printf '%0*d\n' ${#max} $page)
-  set -- $(curl -L -sS "$1" | jq '.' | tee rpde-$page_padded.json | jq -r '.next, (.items | length)')
+  set -- $(curl -L -sS "$1" | jq "$jq_program" | tee rpde-$page_padded.json | jq -r '.next, (.items | length)')
   printf 'got page with next url: %s, num items: %s\n' "$1" $2
   case $1 in /*)
     set -- $base$1 $2
