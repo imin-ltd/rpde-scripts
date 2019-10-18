8b52a912d973643c2d0634da049f47ef0af121ca
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2019-10-18 19:21:38 +0100

    Allow prefix to be set, to facilitate multiple feeds and output directories

--- walk-rpde.sh
+++ walk-rpde.sh
@@ -8,21 +8,22 @@ set -o pipefail
 
 # required commands: awk, curl, jq, tee
 
-if [ "$#" -lt "1" -o "$#" -gt "2" ]
+if [ "$#" -lt "2" -o "$#" -gt "3" ]
 then
-  printf 'Usage: %s <rpde-endpoint> [-s]\n' "$0"
+  printf 'Usage: %s <${prefix}-rpde-endpoint> <output-filename-prefix> [-s]\n' "$0"
   printf '\n'
-  printf ' -s  Squashes all pages into a single page, "rpde.json"\n'
+  printf ' -s  Squashes all pages into a single page, "{prefix}-rpde.json"\n'
   exit 1
 fi
 
-single_file="${2:-}"
-max=100
+single_file="${3:-}"
+max=1000
 page=1
 base=$(printf '%s' "$1" | awk -F/ '{print $1 "//" $3}')
 
 last_next_url=
 next_url="$1"
+prefix="$2"
 num_items=-1
 
 echo "Filenames" > index.csv
@@ -33,8 +34,8 @@ do
   page_padded=$(printf '%0*d\n' ${#max} ${page})
   last_next_url="${next_url}"
   # Results of $() statement are stored in next_url & num_items
-  read -r next_url num_items <<< $(curl -L -sS "${next_url}" | jq '.' | tee "rpde-${page_padded}.json" | jq -r '.next, (.items | length)')
-  echo "rpde-${page_padded}.json" >> index.csv
+  read -r next_url num_items <<< $(curl -L -sS "${next_url}" | jq '.' | tee "${prefix}-rpde-${page_padded}.json" | jq -r '.next, (.items | length)')
+  echo "${prefix}-rpde-${page_padded}.json" >> index.csv
   printf 'got page: %s, with next url: %s, num items: %s\n' "${page}" "${next_url}" "${num_items}"
   # If "next" URL isn't an absolute URL, like /rpde?afterTimestamp=123&afterId=abc, prepend the base URL to it to create an absolute URL
   case ${next_url} in /*)
@@ -45,7 +46,7 @@ done
 
 if [ "${single_file}" == "-s" ]
 then
-  jq -s '[.[].items[]]' rpde-*.json > rpde.json
-  rm -rf rpde-*.json
+  jq -s '[.[].items[]]' ${prefix}-rpde-*.json > ${prefix}-rpde.json
+  rm -rf ${prefix}-rpde-*.json
   rm index.csv
 fi
