e5caaee82749e50af8f00bfc54eb0e4a4d617efa
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2019-10-19 09:18:57 +0100

    Accept API key (to allow use with imin Firehose API)

--- walk-rpde.sh
+++ walk-rpde.sh
@@ -8,22 +8,23 @@ set -o pipefail
 
 # required commands: awk, curl, jq, tee
 
-if [ "$#" -lt "2" -o "$#" -gt "3" ]
+if [ "$#" -lt "3" -o "$#" -gt "4" ]
 then
-  printf 'Usage: %s <${prefix}rpde-endpoint> <output-filename-prefix> [-s]\n' "$0"
+  printf 'Usage: %s <rpde-endpoint> <api-key> <output-filename-prefix> [-s]\n' "$0"
   printf '\n'
-  printf ' -s  Squashes all pages into a single page, "{prefix}-rpde.json"\n'
+  printf ' -s  Squashes all pages into a single page, "{output-filename-prefix}rpde.json"\n'
   exit 1
 fi
 
-single_file="${3:-}"
+single_file="${4:-}"
 max=1000
 page=1
 base=$(printf '%s' "$1" | awk -F/ '{print $1 "//" $3}')
 
 last_next_url=
 next_url="$1"
-prefix="$2"
+api_key="$2"
+prefix="$3"
 num_items=-1
 
 echo "Filenames" > ${prefix}index.csv
@@ -34,7 +35,7 @@ do
   page_padded=$(printf '%0*d\n' ${#max} ${page})
   last_next_url="${next_url}"
   # Results of $() statement are stored in next_url & num_items
-  read -r next_url num_items <<< $(curl -L -sS "${next_url}" | jq '.' | tee "${prefix}rpde-${page_padded}.json" | jq -r '.next, (.items | length)')
+  read -r next_url num_items <<< $(curl -L -sS "${next_url}" --header "X-API-KEY: ${api_key}" | jq '.' | tee "${prefix}rpde-${page_padded}.json" | jq -r '.next, (.items | length)')
   echo "${prefix}rpde-${page_padded}.json" >> ${prefix}index.csv
   printf 'got page: %s, with next url: %s, num items: %s\n' "${page}" "${next_url}" "${num_items}"
   # If "next" URL isn't an absolute URL, like /rpde?afterTimestamp=123&afterId=abc, prepend the base URL to it to create an absolute URL
